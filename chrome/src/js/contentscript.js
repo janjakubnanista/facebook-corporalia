79cf2a7d89102021a50b110ee5319612317dfe99
Jan Nanista <JNanista@genscape.com>
2017-01-20 20:59:45 +0100

    Put all changes together in contentscript.js

--- chrome/src/js/contentscript.js
+++ chrome/src/js/contentscript.js
@@ -1,94 +1,54 @@
-import q from 'q';
-
+import DOMChangeObserver from 'utils/DOMChangeObserver';
+import DummyLanguageDetector from 'utils/DummyLanguageDetector';
+import EmojiReplacer from 'utils/EmojiReplacer';
+import LanguageDetector from 'utils/LanguageDetector';
 import Languages from 'utils/Languages';
 import Settings from 'utils/Settings';
 import WordGenerator from 'utils/WordGenerator';
 
-const FACEBOOK_EMOTICON_URL_PATTERN = /^https:\/\/(www\.facebook\.com|static\.xx\.fbcdn\.net)\/images\/emoji\.php.*/i;
-
-function emoticonNodeFilter(node) {
-  const tagName = node.tagName.toLowerCase();
-  if (tagName !== 'img') return NodeFilter.FILTER_SKIP;
-
-  const src = node.getAttribute('src');
-  if (!FACEBOOK_EMOTICON_URL_PATTERN.test(src)) return NodeFilter.FILTER_SKIP;
-
-  return NodeFilter.FILTER_ACCEPT;
+function checkEnabled() {
+  return Settings.get().then(({ enabled }) => enabled);
 }
 
-function replaceTextEmoticon(node, word) {
-  if (!node || !node.parentNode || !node.parentNode.parentNode) return;
+function getLanguageDetector() {
+  return Languages.getSelectedLanguageCode().then(languageCode => {
+    if (languageCode === 'auto') {
+      const defaultLanguageCode = Languages.getFirstSupportedFallbackLanguageCode();
 
-  const replacement = document.createElement('span');
-  replacement.appendChild(document.createTextNode(` ${word} `));
+      return new LanguageDetector(defaultLanguageCode);
+    }
 
-  const sibling = node.nextSibling;
-  const parent = node.parentNode;
-  const isWrapped = parent.childNodes.length === 2 && sibling instanceof HTMLElement && sibling.tagName.toLowerCase() === 'span';
-
-  const wrapper = isWrapped ? node.parentNode : node;
-  wrapper.parentNode.insertBefore(replacement, wrapper);
-  wrapper.parentNode.removeChild(wrapper);
-}
-
-function replaceTextEmoticons(node, generator) {
-  const walker = document.createTreeWalker(node, NodeFilter.SHOW_ELEMENT, {
-    acceptNode: emoticonNodeFilter
-  }, false);
-
-  const emoticonNodes = [];
-  while (walker.nextNode()) {
-    emoticonNodes.push(walker.currentNode);
-  }
-
-  emoticonNodes.forEach(emoticonNode => replaceTextEmoticon(emoticonNode, generator.next()));
-}
-
-function ensureAvailable() {
-  return Settings.areAvailable() ? q.resolve() : q.reject();
-}
-
-function ensureEnabled() {
-  return Settings.get().then(({ enabled }) => {
-    if (!enabled) return q.reject();
+    return new DummyLanguageDetector(languageCode);
   });
-}
-
-function getLanguage() {
-  return Languages.getSelected().then(selectedLanguageCode => {
-    if (selectedLanguageCode === 'auto') return Languages.detect(document);
-
-    return Languages.find(selectedLanguageCode);
-  });
-}
-
-function initialize(language) {
-  const MutationObserverImplemenation = MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-  const generator = new WordGenerator(language.code);
-  const observer = new MutationObserverImplemenation(mutations => {
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(node => replaceTextEmoticons(node, generator));
-    });
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-
-  replaceTextEmoticons(document.body, generator);
 }
 
 function reportError(error) {
-  console.error('Mean Facebook addon has failed with an error:'); // eslint-disable-line no-console
-  console.error({ message: error.message, stack: error.stack }); // eslint-disable-line no-console
+  console.error('Mean Facebook addon has crashed!'); // eslint-disable-line no-console
+
+  if (error) {
+    console.error('Error details:'); // eslint-disable-line no-console
+    console.error(error.message); // eslint-disable-line no-console
+    console.error(error.stack); // eslint-disable-line no-console
+  }
 }
 
-function reportUnavailable() {
-  console.error('Mean Facebook addon needs storage API in order to work.'); // eslint-disable-line no-console
+// First check if the storage (chrome.storage.local or browser.storage.local)
+// is available. If not just exit screaming
+if (!Settings.areAvailable()) {
+  throw new Error('Mean Facebook addon needs storage API in order to work');
 }
 
-ensureAvailable()
-  .then(ensureEnabled, reportUnavailable)
-  .then(getLanguage)
-  .then(initialize, reportError);
+checkEnabled()
+  .then(enabled => {
+    if (!enabled) return null;
+
+    return getLanguageDetector().then(detector => {
+      const generator = new WordGenerator();
+      const replacer = new EmojiReplacer(detector, generator);
+      const observer = new DOMChangeObserver(replacer);
+
+      replacer.replaceWithin(document.body);
+      observer.observe();
+    });
+  })
+  .done(null, reportError);
