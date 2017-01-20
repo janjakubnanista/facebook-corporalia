5e7cf08aab3c6a110e2a8ca80bb1b136b221e899
Jan Nanista <JNanista@genscape.com>
2017-01-20 20:59:15 +0100

    WordGenerator.next() receives language code as the only argument

--- chrome/src/js/utils/WordGenerator.js
+++ chrome/src/js/utils/WordGenerator.js
@@ -3,15 +3,13 @@ import Languages from './Languages';
 const randomInteger = N => Math.floor(Math.random() * N);
 
 export default class WordGenerator {
-  constructor(languageCode) {
-    this.__languageCode = languageCode;
+  constructor() {
     this.__lastUsed = Languages.getAvailable().reduce((hash, language) => Object.assign(hash, {
       [language.code]: null
     }));
   }
-  next() {
-    const languageCode = this.__languageCode;
-    const language = Languages.getAvailable().find(({ code }) => code === languageCode);
+  next(languageCode) {
+    const language = Languages.find(languageCode);
     const words = language.words;
     const numWords = words.length;
     const lastUsedIndex = this.__lastUsed[languageCode];
